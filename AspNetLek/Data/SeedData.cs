93c2439d9839b09076c60c12c21c337a58c46825
Marcus Backholm <Marcus.Backholm@live.se>
2021-04-14 17:02:25 +0200

    Uppdaterade tools från 5.0.4 till 5.0.5

--- AspNetLek/Data/SeedData.cs
+++ AspNetLek/Data/SeedData.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using AspNetLek.Data;
+using System;
 using AspNetLek.Models;
 using System.Linq;
 
